4598b095ca3c497d40479d73cddeae045e2680e3
Evgeni Kirichenko <avikon3412@yandex.ru>
2022-12-20 09:47:58 +0300

    создал

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Ints calc = new IntsCalculator();
+        System.out.println(calc.sum(10,12));
+        System.out.println(calc.mult(10,12));
+        System.out.println(calc.pow(10,3));
+    }
+}
